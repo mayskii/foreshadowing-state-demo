706f9f20f6fab108b9153b718c137c99a622992d
Maya Zakharova <mayzakharova@gmail.com>
2025-11-14 21:59:33 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,66 @@
+// const state = {
+//   crabCount: 0
+// };
+
+// const addCrab = (event) => {
+//   const newCrab = document.createElement('span');
+//   const crabContainer = document.querySelector('#crabContainer');
+//   newCrab.textContent = '🦀';
+//   crabContainer.appendChild(newCrab);
+
+//   // Crab Count Behavior
+//   state.crabCount += 1;
+//   const crabCountContainer = document.querySelector('#crabCount');
+//   crabCountContainer.textContent = `Crab Count: ${state.crabCount}`;
+// };
+
+// const registerEventHandlers = (event) => {
+//   const crabButton = document.querySelector('#addCrabButton');
+//   crabButton.addEventListener('click', addCrab);
+// };
+
+// document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
 const state = {
-  crabCount: 0
+  crabCounter: 0
 };
 
-const addCrab = (event) => {
+const addCrab = () => {
   const newCrab = document.createElement('span');
-  const crabContainer = document.querySelector('#crabContainer');
   newCrab.textContent = '🦀';
+  const crabContainer = document.getElementById('crabContainer');
   crabContainer.appendChild(newCrab);
 
-  // Crab Count Behavior
-  state.crabCount += 1;
+  state.crabCounter += 1;
   const crabCountContainer = document.querySelector('#crabCount');
-  crabCountContainer.textContent = `Crab Count: ${state.crabCount}`;
+  crabCountContainer.textContent = `Crab Count: ${state.crabCounter}`;
 };
 
-const registerEventHandlers = (event) => {
-  const crabButton = document.querySelector('#addCrabButton');
-  crabButton.addEventListener('click', addCrab);
+const crabButton = document.querySelector('#addCrabButton');
+crabButton.addEventListener('click', addCrab);
+
+
+const changeMode = () => {
+  const body = document.body;
+  if (body.classList.contains('dark-mode')) {
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
+  } else {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
+  }
+
 };
 
-document.addEventListener('DOMContentLoaded', registerEventHandlers);
+const modeButton = document.querySelector('#changeModeButton');
+modeButton.addEventListener('click', changeMode);
+
+const deleteMode = () => {
+  const body = document.body;
+  body.classList.remove('light-mode');
+  body.classList.remove('dark-mode');
+};
+
+
+const deleteModeButton = document.querySelector('#deleteModeButton');
+deleteModeButton.addEventListener('click', deleteMode);
